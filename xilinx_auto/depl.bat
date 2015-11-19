78fb4d34d46c58723ae44f0c34e0291474aa24dc
Anton Gulkevich <ycykensmit@gmail.com>
2015-11-19 18:19:41 +0300

    done boot gen done file struct

--- xilinx_auto/depl.bat
+++ xilinx_auto/depl.bat
@@ -2,7 +2,7 @@
 goto start
 ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 Deployment batch file
-last changes 18.11.2015
+last changes 19.11.2015
 ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 ::D:\ubs\Dev\lib\win32DLib\win32DLib\ucu_fw
 ::D:\x-ws\ubs_work.sdk\ubs_design_wrapper.hdf
@@ -22,6 +22,11 @@ if not exist %XSCTDEST% (
 	pause
     exit /b 1
 )
+::set local parameters--------------------------------------------------------------------------------------------------------------
+set "TCLFILE=xcomp.tcl"
+set "MAKEFILE=makeFile.tcl"
+set "HWPNAME=ubs_design_wrapper_hw_platform_0"
+set "ELFFILE=%WORKSPACE%%PROJECTNAME%\Debug\%PROJECTNAME%.elf"
 ::clear previous workspace----------------------------------------------------------------------------------------------------------
 echo clear previous workspace
 FOR /D %%p IN (%WORKSPACE%*.*) DO rmdir "%%p" /s /q
@@ -31,15 +36,12 @@ echo sdk create_hw_project -name %HWPNAME% -hwspec %HDFFILE% >> %TCLFILE%
 echo sdk create_bsp_project -name %PROJECTNAME%_bsp -hwproject %HWPNAME% -proc ps7_cortexa9_0 -os standalone >> %TCLFILE%
 echo sdk create_app_project -name %PROJECTNAME% -hwproject %HWPNAME% -bsp %PROJECTNAME%_bsp -proc ps7_cortexa9_0 -os standalone -lang C -app {Empty Application}  >> %TCLFILE%
 echo exit >> %TCLFILE%
-::create tcl file-------------------------------------------------------------------------------------------------------------------
-
 timeout /t 1 /nobreak > NUL
-
 ::execute tcl file------------------------------------------------------------------------------------------------------------------
-echo execute tcl file %TCLFILE%
+echo executing tcl file %TCLFILE%
 call %XSCTDEST% %TCLFILE%
 ::copy source files-----------------------------------------------------------------------------------------------------------------
-xcopy /s/e/y/h "D:\ubs\Dev\lib\win32DLib\win32DLib\ucu_fw" "D:\ws\ucu_fw"
+xcopy /s/e/y/h %CRCFILES% %WORKSPACE%\%PROJECTNAME%
 ::build elf file--------------------------------------------------------------------------------------------------------------------
 echo sdk set_workspace %WORKSPACE% > makeFile.tcl
 echo sdk build_project  >> makeFile.tcl
@@ -47,7 +49,6 @@ echo exit >> makeFile.tcl
 timeout /t 1 /nobreak > NUL
 call %XSCTDEST% makeFile.tcl
 ::check file existance--------------------------------------------------------------------------------------------------------------
-set "ELFFILE=%WORKSPACE%%PROJECTNAME%\Debug\%PROJECTNAME%.elf"
 echo %ELFFILE%
 :checkExist
 if not exist %ELFFILE% (
@@ -66,8 +67,121 @@ if exist %ELFFILE% (
 :isUnloked
 rename %ELFFILE% %ELFFILE%
 if not errorlevel 0 goto isUnloked
+echo %ELFFILE% is ready to process
+::creating biff file----------------------------------------------------------------------------------------------------------------
+echo creating boot.bin
 
-::placeholder to use *.elf file-----------------------------------------------------------------------------------------------------
+echo the_ROM_image: > output.bif
+echo { >> output.bif
+echo [bootloader]%BOOTLOADER% >> output.bif
+echo %WORKSPACE%%HWPNAME%\ubs_design_wrapper.bit >> output.bif
+echo %ELFFILE% >> output.bif
+echo } >> output.bif
 
-echo %ELFFILE% is ready
+timeout /t 1 /nobreak > NUL
+::boot gen -------------------------------------------------------------------------------------------------------------------------
+call %BOOTGENFILE% -image output.bif -o i %WORKSPACE%boot.bin -w
+:checkBootFile
+if not exist %WORKSPACE%boot.bin (
+	timeout /t 1 /nobreak > NUL
+	goto checkBootFile
+)
+echo %WORKSPACE%boot.bin created successfully
+::creating file structure-----------------------------------------------------------------------------------------------------------
+::disk
+::	bin
+::		TUKN.00114-05_JTAG.exe
+::			burn.bat
+::			BOOT.bin
+::		TUKN.00114-05_USB.exe
+::			fw-c.exe
+::			burn.bat
+::			BOOT.bin
+::	doc
+::		Инструкция по загрузке .docx
+::		Лист утверждения ПO.docx
+::		Лист утверждения ПO.docx
+::		Спецификация.docx
+::		Текст программ.docx
+::		ТЗ тестовое Разбор и вывод данных записанных с приемника цифрового сигнала.docx
+::		ТЗ убс ПЛИС.docx
+::	pcfw
+::		CyUSB.dll
+::		DeviceManager.dll
+::		FTDDeviceManager.dll
+::		imit_BPUTester.dll
+::		MahApps.Metro.dll
+::		MMTimer.dll
+::		OxyPlot.dll
+::		OxyPlot.Wpf.dll
+::		OxyPlot.Xps.dll
+::		SCC_468173_007.dll
+::		SccChannelControl.dll
+::		SCContracts.dll
+::		SCDeviceInterface.dll
+::		SCDeviceManager.dll
+::		SCStreams.dll
+::		SCVirtualChannelDevice.dll
+::		System.Windows.Interactivity.dll
+::		UBSApp.dll
+::		UBSLib.dll
+::		UBSManager.exe
+::		СomposeContracts.dll
+::	src
+::		pld.exe
+::		soft.exe
+::			ucu_fw
+::			ucu_fw_bsp
+::	info.txt
+::creating structure----------------------------------------------------------------------------------------------------------------
+mkdir %WORKSPACE%disk
+mkdir %WORKSPACE%disk\bin
+mkdir %WORKSPACE%disk\doc
+mkdir %WORKSPACE%disk\pcwf
+mkdir %WORKSPACE%disk\src
+::create temp dir-------------------------------------------------------------------------------------------------------------------
+set "TEMPDIR=%WORKSPACE%tempDir"
+mkdir %TEMPDIR%
+::make rar.exe files--------------------------------------------------------------------------------------------------------------- 
+if exist "%Programfiles%\winrar\winrar.exe"
+	set WINRARDIST=%Programfiles%\winrar\winrar.exe
+	goto packWinRar
+)
+set WINRARDIST=%Programfiles% (x86)\winrar\winrar.exe
+:packWinRar
+if not exist %WINRARDIST%(
+    echo FAIL: pls install winrar in %WINRARDIST%
+	pause
+    exit /b 1
+)
+::creating autorun.txt--------------------------------------------------------------------------------------------------------------
+set AUTORUNTXT=autorun.txt
+echo TempMode=Будет проведено программирование блока УБС\nпрограммым обеспечением ТЮКН.00114-05. Продолжить?,Программное обеспечение ТЮКН.00114-05 > %AUTORUNTXT%
+echo Setup=burn.bat >> %AUTORUNTXT%
+::----------------------------------------------------------------------------------------------------------------------------------
+
+
+%WINRARDIST% a -sfx -zautorun.txt -m5 TUKN.00114-05_JTAG.exe @files.lst
+::copy files-----------------------------------------------------------------------------------------------------------------------
+xcopy /s/e/y/h %WORKSPACE%disk\bin %BINDESCT%
+xcopy /s/e/y/h %WORKSPACE%disk\doc %BINDESCT%
+xcopy /s/e/y/h %WORKSPACE%disk\pcwf %BINDESCT%
+xcopy /s/e/y/h %WORKSPACE%disk\src %BINDESCT%
+xcopy /s/e/y/h %WORKSPACE%disk %INFOTXTFILEDEST%
+::----------------------------------------------------------------------------------------------------------------------------------
+::create folders
+::copy files
+
+
+
+
+
+
+
+
+
+
+
+
+
 pause
--- xilinx_auto/var.bat
+++ xilinx_auto/var.bat
@@ -5,10 +5,72 @@ Configuration file with local variables
 last changes 18.11.2015
 ——————————————————————————————————————
 :start2
+::workspace directory need for xilinx sdk 
 set "WORKSPACE=D:\ws\"
+::name of main project
 set "PROJECTNAME=ucu_fw"
+::destination of xilinx command line tool bat file xsct.bat
 set "XSCTDEST=D:\Xilinx\SDK\2015.1\bin\xsct.bat"
-set "TCLFILE=xcomp.tcl"
+::desination of hardware platform file *.hdf
 set "HDFFILE=D:\\x-ws\\ubs_work.sdk\\ubs_design_wrapper.hdf"
-set "HWPNAME=ubs_design_wrapper_hw_platform_0"
-set "MAKEFILE=makeFile.tcl"
+::destination of loader.elf file
+set "BOOTLOADER=D:\scripts\loader.elf"
+::name of bit file needed for boot.bin compilation
+set "UBSDESIGNWRAPPER=ubs_design_wrapper.bit"
+::destination od xilinx bootgen.bat file
+set "BOOTGENFILE=D:\Xilinx\SDK\2015.1\bin\bootgen.bat"
+::destination of source files
+set "CRCFILES=D:\ubs\Dev\lib\win32DLib\win32DLib\ucu_fw"
+::	bin
+::		TUKN.00114-05_JTAG.exe
+::			burn.bat
+::			BOOT.bin
+::		TUKN.00114-05_USB.exe
+::			fw-c.exe
+::			burn.bat
+::			BOOT.bin
+set "BINDESCT="
+::---------------------------------------------------------------------------------------
+::destination of documents
+::	doc
+::		Инструкция по загрузке .docx
+::		Лист утверждения ПO.docx
+::		Лист утверждения ПO.docx
+::		Спецификация.docx
+::		Текст программ.docx
+::		ТЗ тестовое Разбор и вывод данных записанных с приемника цифрового сигнала.docx
+::		ТЗ убс ПЛИС.docx
+set "DOCDEST="
+::---------------------------------------------------------------------------------------
+::	pcfw
+::		CyUSB.dll
+::		DeviceManager.dll
+::		FTDDeviceManager.dll
+::		imit_BPUTester.dll
+::		MahApps.Metro.dll
+::		MMTimer.dll
+::		OxyPlot.dll
+::		OxyPlot.Wpf.dll
+::		OxyPlot.Xps.dll
+::		SCC_468173_007.dll
+::		SccChannelControl.dll
+::		SCContracts.dll
+::		SCDeviceInterface.dll
+::		SCDeviceManager.dll
+::		SCStreams.dll
+::		SCVirtualChannelDevice.dll
+::		System.Windows.Interactivity.dll
+::		UBSApp.dll
+::		UBSLib.dll
+::		UBSManager.exe
+::		СomposeContracts.dll
+set "PCFWDEST="
+::---------------------------------------------------------------------------------------
+::	src
+::		pld.exe
+::		soft.exe
+::			ucu_fw
+::			ucu_fw_bsp
+set "CRCDEST="
+::---------------------------------------------------------------------------------------
+set "INFOTXTFILEDEST="
